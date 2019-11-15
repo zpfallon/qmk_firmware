99acce80d6a5ae3c9ff1427b26e061ecc915ea99
Zachary Fallon <zfallon@marathonus.com>
2019-11-15 12:05:54 -0500

    easier alt tab

--- keyboards/gergo/keymaps/zach/keymap.c
+++ keyboards/gergo/keymaps/zach/keymap.c
@@ -42,8 +42,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [BASE] = LAYOUT_gergo(
     LT(NUMB, KC_ESC),       KC_Q,  KC_W,   KC_E,   KC_R, KC_T,                                            KC_Y,    KC_U, KC_I, KC_O,   KC_P,    KC_BSLASH,
-    MT(MOD_LCTL, KC_BSPC),  KC_A,  KC_S,   KC_D,   KC_F, KC_G, KC_BTN2,                         KC_PGUP,  KC_H,    KC_J, KC_K, KC_L,   KC_SCLN, KC_QUOT,
-    KC_LSFT,                KC_Z,  KC_X,   KC_C,   KC_V, KC_B, KC_BTN1, KC_BTN3,       KC_LGUI, KC_PGDN,  KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_MINS,
+    MT(MOD_LCTL, KC_BSPC),  KC_A,  KC_S,   KC_D,   KC_F, KC_G, KC_TAB,                         KC_PGUP,  KC_H,    KC_J, KC_K, KC_L,   KC_SCLN, KC_QUOT,
+    KC_LSFT,                KC_Z,  KC_X,   KC_C,   KC_V, KC_B, LSFT(KC_TAB), KC_BTN3,       KC_LGUI, KC_PGDN,  KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_MINS,
     LT(NUMB, KC_ESC), LT(SYMB, KC_SPC), MT(MOD_LGUI, KC_DEL), MT(MOD_LALT, KC_ENT),    KC_TAB, KC_BSPC, LT(NUMB, KC_SPC), LT(SYMB, KC_ENT)
 ),
 /* Keymap 1: Symbols layer
